3ac0a1fff2c55d487618891a4a0108798e2a8d09
Suphawadee <ked.suphawadee@gmail.com>
2021-06-16 21:32:43 +0700

    update select

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
-import React from "react";
-import PropTypes from "prop-types";
-import Head from "next/head";
-import "../styles/globals.css";
+import React from 'react';
+import PropTypes from 'prop-types';
+import Head from 'next/head';
+
+import '../styles/globals.css';
+import 'antd-table-infinity/index.css';
 
 export default function MyApp(props) {
   const { Component, pageProps } = props;
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
-import Select from 'react-select';
+import dynamic from 'next/dynamic';
 
 import { httpClient } from '../utils/HttpClient';
 import styles from '../styles/Home.module.css';
 
+const SelectNoSSR = dynamic(() => import("react-select"), { ssr: false });
+
 const Home = (props) => {
 
-  const [countries, setCountries] = useState({label:"Afghanistan", value: "Afghanistan"});
+  const [countries, setCountries] = useState('');
   const [data, setData] = useState([]);
   const [isSearchable, setIsSearchable] = useState(countries);
+  const [page, setPage] = useState(1);
+  const [dataTable, setDataTable] = useState([]);
 
   useEffect(() => {
     getCountry();
@@ -19,12 +23,8 @@ const Home = (props) => {
   const getCountry = async () => {
     await httpClient.get(`/countries`)
     .then((res) => {
-      console.log('res', res.data)
-      const obj = Object.keys(res.data.response).map((val, key) => {
-        console.log(key, val)
-        ({ id:key, label: key, value: key })
-      });
-      
+      const obj = res.data.response.map((item, index) => ({ id: index+1, label: item, value: item }))
+      // console.log(obj);
       setCountries(obj);
     })
     .catch((err) => console.log('err', err));
@@ -39,10 +39,44 @@ const Home = (props) => {
     await httpClient.get(`/statistics?country=${country}`)
     .then((res) => {
       console.log('res', res);
-      setData(res)
+      setData(res.data)
     })
     .catch((err) => console.log('err', err));
   };
+
+  const column = [
+    {
+      key: 'name',
+      title: 'Name',
+      dataIndex: 'name',
+    },
+    {
+      key: 'cumulative',
+      title: 'Cases - cumulative total',
+      dataIndex: 'cumulative',
+    },
+    {
+      key: 'newly',
+      title: 'Cases - newly reported in last 24 hours',
+      dataIndex: 'newly',
+    },
+    {
+      key: 'deaths_total',
+      title: 'Deaths - cumulative total',
+      dataIndex: 'deaths_total',
+    },
+    {
+      key: 'deaths',
+      title: 'Deaths - newly reported in last 24 hours',
+      dataIndex: 'deaths',
+    },
+    {
+      key: 'transmission',
+      title: 'Transmission Classification',
+      dataIndex: 'transmission',
+    }
+  ]
+
   return (
     <div className={styles.container}>
       <Head>
@@ -55,12 +89,111 @@ const Home = (props) => {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-        {console.log('countries', countries)}
-        {/* <Select
+        <SelectNoSSR
+          styles={{width: 200}}
+          instanceId="instance-id"
           value={isSearchable}
           onChange={toggleSearchable}
           options={countries}
-        /> */}
+        />
+        <div className="sc-AxjAm sc-oUDcU drsnSD">
+          <h2 className="sc-AxheI bbtMLp">
+            <div style={{fontSize: 25}}>
+              <div style={{display: 'block', whiteSpace: 'nowrap'}}>
+                Situation by Region, Country, Territory &amp; Area
+              </div>
+            </div>
+          </h2>
+          <div className="sc-pczax hJdRYq">
+            <div
+              role="table"
+              className="sc-fzonjX bXGqiI"
+              style={{minWidth: 0}}
+            >
+              <div className="thead">
+                <div
+                  className="tr"
+                  role="row"
+                >
+                  {
+                    column.map((item) => (
+                      <div
+                        key={item.key}
+                        className="th"
+                        colSpan={1}
+                        role="columnheader"
+                        title="Toggle SortBy" 
+                        // style={{
+                        //   boxSizing: 'border-box',
+                        //   flex: '82.3333 0 auto',
+                        //   minWidth: 0,
+                        //   width: '82.3333px',
+                        //   cursor: 'pointer'
+                        // }}
+                      >
+                        <div className="sc-AxjAm kbGAkV">
+                          <span>{item.title}</span>
+                          <span />
+                        </div>
+                      </div>
+                    ))
+                  }
+                </div>
+              </div>
+              <div data-id="totals-header" className="sc-fznOgF glfmCu">
+                <div className="tr">
+                  <div className="sc-fznYue iNYtic th" colSpan={1} style={{boxSizing: 'border-box', flex: '82.3333 0 auto', minWidth: 0, width: '82.3333px'}}>
+                    <strong>Global</strong>
+                  </div>
+                  <div className="sc-fznYue joCDVo th" colSpan={1} style={{boxSizing: 'border-box', flex: '82.3333 0 auto', minWidth: 0, width: '82.3333px'}}>
+                    176,156,662
+                  </div>
+                  <div className="sc-fznYue iNYtic th" colSpan={1} style={{boxSizing: 'border-box', flex: '82.3333 0 auto', minWidth: 0, width: '82.3333px'}}>
+                    292,499
+                  </div>
+                  <div className="sc-fznYue iNYtic th" colSpan={1} style={{boxSizing: 'border-box', flex: '82.3333 0 auto', minWidth: 0, width: '82.3333px'}}>
+                    3,815,486
+                  </div>
+                  <div className="sc-fznYue iNYtic th" colSpan={1} style={{boxSizing: 'border-box', flex: '82.3333 0 auto', minWidth: 0, width: '82.3333px'}}>
+                    8,082
+                  </div>
+                  <div className="sc-fznYue iNYtic th" colSpan={1} style={{boxSizing: 'border-box', flex: '82.3333 0 auto', minWidth: 0, width: '82.3333px'}} />
+                  </div>
+                </div>
+              <div role="rowgroup" className="tbody">
+                <div style={{position: 'relative', height: 600, width: 1008, overflow: 'visible auto', willChange: 'transform', direction: 'ltr'}}>
+                  <div style={{height: 50, width: '100%'}}>
+                    <div role="row" className="tr depth_0 " style={{display: 'flex', flex: '1 0 auto', minWidth: 0, position: 'absolute', left: 0, top: 0, height: 50, width: '100%'}}>
+                      {console.log('data', data)}
+                      <div className="column_name td" role="cell" style={{boxSizing: 'border-box', flex: '82.3333 0 auto', minWidth: 0, width: '82.3333px'}}>
+                        <div className="sc-AxjAm sc-fzoMdx cTipgc">
+                          <img src="" alt="flag TH" width={30} height={20} data-id="country-flag" />
+                          <span>Thailand</span>
+                        </div>
+                      </div>
+                      <div className="column_Cumulative_Confirmed td" role="cell" style={{boxSizing: 'border-box', flex: '82.3333 0 auto', minWidth: 0, width: '82.3333px'}}>
+                        <div className="sc-AxjAm sc-fznAgC dWLLcp">
+                          <div className="sc-AxjAm sc-fzowVh CBlYA">
+                            <div className="sc-fzqzEs hULauc">202,264</div>
+                            <div data-id="bar" className="sc-AxjAm sc-fzpkqZ dMCzxG">
+                              <div className="sc-AxjAm sc-fznLPX eshEoa bar_fill" style={{width: '0.610117%'}}>
+                                <div className="sc-AxjAm sc-fzqMdD eBAmNl" /></div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="column_Confirmed td" role="cell" style={{boxSizing: 'border-box', flex: '82.3333 0 auto', minWidth: 0, width: '82.3333px'}}>
+                          <div className="sc-fzqzEs hULauc">3,000</div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          
+
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
